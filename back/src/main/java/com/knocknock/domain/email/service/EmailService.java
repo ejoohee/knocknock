cf7e6fad0a570077d5e923288fe9fed4e024cb99
Yoonoi <dbddbd39@naver.com>
2023-10-20 18:58:10 +0900

    Fix email : 로직 수정 (Controller -> service 로직 이동)

--- back/src/main/java/com/knocknock/domain/email/service/EmailService.java
+++ back/src/main/java/com/knocknock/domain/email/service/EmailService.java
@@ -1,5 +1,9 @@
-package com.knocknock.domain.email;
+package com.knocknock.domain.email.service;
 
+import com.knocknock.domain.email.domain.EmailMessage;
+import com.knocknock.domain.email.dto.EmailCodeResDto;
+import com.knocknock.domain.email.dto.EmailPostDto;
+import com.knocknock.domain.email.exception.EmailCodeException;
 import com.knocknock.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,9 +26,16 @@ public class EmailService {
     private final JavaMailSender javaMailSender;
     private final UserService userService;
 
-    public String sendEmail(EmailMessage emailMessage, String type) {
+    public EmailCodeResDto sendEmail(EmailPostDto emailPostDto, String type) {
+        log.info("[이메일 발신] 발신 요청. email : {}, type : {}", emailPostDto.getEmail(), type);
+
+        // 요청 타입별 이메일 제목 및 수신자 설정
+        EmailMessage emailMessage = setEmailSubject(emailPostDto, type);
+
+        // 임시 코드 발급
         String authNum = createCode();
 
+        // 메일발송을 위한 메일 생성
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
         // 패스워드 찾기면 해당 유저의 비밀번호를 임시 비밀번호로 변경
@@ -35,22 +46,47 @@ public class EmailService {
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
             helper.setTo(emailMessage.getUserEmail());  // 메일 수신자 설정
             helper.setSubject(emailMessage.getSubject()); // 메일 제목 설정
-            helper.setText(setContext(authNum, type), true);  // 메일 내용, HTML 여부
+            helper.setText(setContext(authNum, type), true);  // 메일 내용, HTML 여부(타임리프를 사용해서 true)
 
             javaMailSender.send(mimeMessage);
-            log.info("[이메일 코드 발신] 이메일 발신 성공! email : {}", emailMessage.getUserEmail());
+            log.info("[이메일 발신] 이메일 발신 성공! email : {}, code : {}", emailMessage.getUserEmail(), authNum);
 
-            return authNum;
+            return EmailCodeResDto.builder()
+                    .emailCode(authNum)
+                    .build();
         } catch (MessagingException e) {
-            log.error("[이메일 코드 발신] 실패");
-//            throw new EmailCodeException("이메일 코드 발신 실패");
-            throw new RuntimeException(e);
+            log.error("[이메일 발신] 발신 실패. ERROR 발생.");
+            throw new EmailCodeException("이메일 발신 실패 에러");
         }
 
     }
 
     /**
+     * 이메일 제목 설정
+     * 요청 타입에 따라 이메일 제목을 설정합니다.
+     * 수신자 이메일도 설정합니다.
+     */
+    public EmailMessage setEmailSubject(EmailPostDto emailPostDto, String type) {
+        log.info("[이메일 제목 설정] type : {}", type);
+
+        String subject = "";
+
+        // 비밀번호 찾기라면
+        if(type.equals("password")) {
+            subject = "[KnocknocK] (비밀번호 찾기) 임시 비밀번호 발급";
+        } else {
+            subject = "[KnocknocK] (회원가입) 이메일 인증 코드 발송";
+        }
+
+        return EmailMessage.builder()
+                .userEmail(emailPostDto.getEmail())
+                .subject(subject)
+                .build();
+    }
+
+    /**
      * 인증번호 및 임시 비밀번호 생성
+     * 랜덤으로 코드를 생성합니다.
      */
     public String createCode() {
         Random random = new Random();
