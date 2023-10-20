ab9167390f993557c14e54d77abb717c4b4082aa
Yoonoi <dbddbd39@naver.com>
2023-10-20 21:35:51 +0900

    Fix Email : service 로직 수정

--- back/src/main/java/com/knocknock/domain/email/service/EmailService.java
+++ back/src/main/java/com/knocknock/domain/email/service/EmailService.java
@@ -1,12 +1,16 @@
 package com.knocknock.domain.email.service;
 
 import com.knocknock.domain.email.domain.EmailMessage;
+import com.knocknock.domain.email.dto.EmailCodeReqDto;
 import com.knocknock.domain.email.dto.EmailCodeResDto;
 import com.knocknock.domain.email.dto.EmailPostDto;
 import com.knocknock.domain.email.exception.EmailCodeException;
 import com.knocknock.domain.user.service.UserService;
+import com.knocknock.global.exception.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -16,6 +20,7 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -25,40 +30,80 @@ public class EmailService {
     private final SpringTemplateEngine templateEngine; // 타임리프 이용
     private final JavaMailSender javaMailSender;
     private final UserService userService;
+    private final RedisTemplate<String, Object> redisTemplate; // 이메일 코드 저장용
 
     public EmailCodeResDto sendEmail(EmailPostDto emailPostDto, String type) {
-        log.info("[이메일 발신] 발신 요청. email : {}, type : {}", emailPostDto.getEmail(), type);
+        String email = emailPostDto.getEmail();
+        log.info("[이메일 발신] 발신 요청. email : {}, type : {}", email, type);
 
         // 요청 타입별 이메일 제목 및 수신자 설정
-        EmailMessage emailMessage = setEmailSubject(emailPostDto, type);
+        EmailMessage emailMessage = setEmailSubject(email, type);
 
         // 임시 코드 발급
-        String authNum = createCode();
+        String authCode = createCode();
 
         // 메일발송을 위한 메일 생성
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
         // 패스워드 찾기면 해당 유저의 비밀번호를 임시 비밀번호로 변경
         if(type.equals("password"))
-            userService.updateTempPassword(emailMessage.getUserEmail(), authNum);
+            userService.updateTempPassword(email, authCode);
 
         try {
+            // 메일 내용 설정
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-            helper.setTo(emailMessage.getUserEmail());  // 메일 수신자 설정
+            helper.setTo(email);  // 메일 수신자 설정
             helper.setSubject(emailMessage.getSubject()); // 메일 제목 설정
-            helper.setText(setContext(authNum, type), true);  // 메일 내용, HTML 여부(타임리프를 사용해서 true)
+            helper.setText(setContext(authCode, type), true);  // 메일 내용, HTML 여부(타임리프를 사용해서 true)
 
+            // 메일 발송 시도
+            log.info("[이메일 발신] 이메일 작성 완료. 발송 시도");
             javaMailSender.send(mimeMessage);
-            log.info("[이메일 발신] 이메일 발신 성공! email : {}, code : {}", emailMessage.getUserEmail(), authNum);
-
-            return EmailCodeResDto.builder()
-                    .emailCode(authNum)
-                    .build();
         } catch (MessagingException e) {
             log.error("[이메일 발신] 발신 실패. ERROR 발생.");
             throw new EmailCodeException("이메일 발신 실패 에러");
         }
 
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+
+        // 유효시간(10분) 동안 {email, authKey} 저장
+        valueOperations.set(email, authCode, 60 * 10L, TimeUnit.SECONDS);
+
+        log.info("[이메일 발신] 이메일 발신 성공! email : {}, code : {}", email, authCode);
+        return EmailCodeResDto.builder()
+                .emailCode(authCode)
+                .build();
+    }
+
+    /**
+     * 발송된 이메일 인증 코드와, 작성한 인증 코드 일치 체크
+     */
+    public Boolean checkEmailCode(EmailCodeReqDto emailCodeReqDto) {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+
+        // email 키로 code value 반환
+        String originCode = valueOperations.get(emailCodeReqDto.getEmail()).toString();
+
+        if(originCode == null){
+            log.error("[인증 코드 일치 체크] 인증 코드가 존재하지 않습니다.");
+            throw new NotFoundException("해당 이메일로 유효한 인증 코드가 존재하지 않습니다.");
+        }
+
+        if(originCode.equals(emailCodeReqDto.getCode())) {
+            log.info("[인증 코드 일치 체크] 코드 일치. 이메일 인증 완료.");
+
+            // 레디스에 저장해놨던 임시 코드 삭제 후
+            valueOperations.getOperations().delete(emailCodeReqDto.getEmail());
+
+            // 인증 완료를 저장
+            valueOperations.set(emailCodeReqDto.getEmail(), "인증완료", 60 * 10L, TimeUnit.SECONDS);
+
+            return true;
+        }
+
+        // 이거 코드 잘못입력한 후 false 나오고 나면
+        // 그이후에 맞는 코드 작성해도 계속 false 나오는데 이게맞나,,,?
+        return false;
     }
 
     /**
@@ -66,7 +111,7 @@ public class EmailService {
      * 요청 타입에 따라 이메일 제목을 설정합니다.
      * 수신자 이메일도 설정합니다.
      */
-    public EmailMessage setEmailSubject(EmailPostDto emailPostDto, String type) {
+    public EmailMessage setEmailSubject(String email, String type) {
         log.info("[이메일 제목 설정] type : {}", type);
 
         String subject = "";
@@ -79,7 +124,7 @@ public class EmailService {
         }
 
         return EmailMessage.builder()
-                .userEmail(emailPostDto.getEmail())
+                .userEmail(email)
                 .subject(subject)
                 .build();
     }
