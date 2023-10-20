70439c9c59960e562ce5ca0af32335227003a1a8
Yoonoi <dbddbd39@naver.com>
2023-10-20 21:38:36 +0900

    Fix User : 메일발송 서비스 -> emailService로 이동

--- back/src/main/java/com/knocknock/domain/user/service/UserService.java
+++ back/src/main/java/com/knocknock/domain/user/service/UserService.java
@@ -2,7 +2,7 @@ package com.knocknock.domain.user.service;
 
 import com.knocknock.domain.user.dto.password.FindPasswordReqDto;
 import com.knocknock.domain.user.dto.password.PasswordReqDto;
-import com.knocknock.domain.user.dto.password.UpdatePasswordRepDto;
+import com.knocknock.domain.user.dto.password.UpdatePasswordReqDto;
 import com.knocknock.domain.user.dto.request.*;
 import com.knocknock.domain.user.dto.response.AdminUserResDto;
 import com.knocknock.domain.user.dto.response.LoginResDto;
@@ -15,10 +15,10 @@ public interface UserService {
 
     void signUp(UserReqDto userReqDto); // 회원가입
     Boolean checkEmail(String email); // 이메일 중복검사
-    void sendEmailCode(String email); // 이메일 인증코드 발신
-    Boolean checkEmailCode(CheckEmailCodeReqDto checkEmailCodeReqDto); // 이메일 인증코드 유효 검사
+//    void sendEmailCode(String email); // 이메일 인증코드 발신 -> emailService
+//    Boolean checkEmailCode(EmailCodeReqDto emailCodeReqDto); // 이메일 인증코드 유효 검사
 
-    void addJiroCode(JiroCodeRepDto jiroCodeRepDto, String token); // 지로 코드 등록
+    void addJiroCode(JiroCodeReqDto jiroCodeRepDto, String token); // 지로 코드 등록
 
     LoginResDto login(LoginReqDto loginReqDto);
     void logout(String token);
@@ -26,7 +26,7 @@ public interface UserService {
     void findPassword(FindPasswordReqDto findPasswordReqDto); // 비밀번호 찾아 이메일 전송하기
     void updateTempPassword(String email, String tempPassword);
     Boolean checkPassword(PasswordReqDto passwordReqDto, String token);  // 서비스전 비밀번호 체크
-    void updatePassword(UpdatePasswordRepDto updatePasswordRepDto, String token); // 비밀번호 변경
+    void updatePassword(UpdatePasswordReqDto updatePasswordRepDto, String token); // 비밀번호 변경
 
 
     UserResDto updateUser(UpdateUserReqDto updateUserReqDto, String token); // 내정보 수정
