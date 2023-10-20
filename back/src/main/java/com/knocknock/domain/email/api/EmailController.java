e7785f3185f9f3cd385659c273eb7e3f6548484c
Yoonoi <dbddbd39@naver.com>
2023-10-20 21:54:28 +0900

    Fix email : service <-> serviceImpl 분리

--- back/src/main/java/com/knocknock/domain/email/api/EmailController.java
+++ back/src/main/java/com/knocknock/domain/email/api/EmailController.java
@@ -3,7 +3,7 @@ package com.knocknock.domain.email.api;
 import com.knocknock.domain.email.dto.EmailCodeReqDto;
 import com.knocknock.domain.email.dto.EmailCodeResDto;
 import com.knocknock.domain.email.dto.EmailPostDto;
-import com.knocknock.domain.email.service.EmailService;
+import com.knocknock.domain.email.service.EmailServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EmailController {
 
-    private final EmailService emailService;
+    private final EmailServiceImpl emailService;
 
     @Operation(
             summary = "회원가입 이메일 인증 코드 발신",
