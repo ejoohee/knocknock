f1e8bbbcbf6d5672320b65ac5076e15600ae5887
조은비 <harunz@naver.com>
2023-10-23 15:29:59 +0900

    Feat(Model) : queryDSL repository 추가

--- back/src/main/java/com/knocknock/domain/model/dao/ModelRepository.java
+++ back/src/main/java/com/knocknock/domain/model/dao/ModelRepository.java
@@ -3,8 +3,7 @@ package com.knocknock.domain.model.dao;
 import com.knocknock.domain.model.domain.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ModelRepository extends JpaRepository<Model, Long> {
-
+public interface ModelRepository extends JpaRepository<Model, Long>, ModelRepositoryCustom {
 
 
 }
