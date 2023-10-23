a7e453d3fc5e1173bf0c7abfb006ae795de0a68d
조은비 <harunz@naver.com>
2023-10-23 11:14:15 +0900

    Feat(Model) : Model Repository 생성

---
+++ back/src/main/java/com/knocknock/domain/model/dao/ModelRepository.java
@@ -0,0 +1,10 @@
+package com.knocknock.domain.model.dao;
+
+import com.knocknock.domain.model.domain.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModelRepository extends JpaRepository<Model, Long> {
+
+
+
+}
