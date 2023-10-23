9bb27105f799a5fc79ec60651a725d9bffe6c4e8
MoonHongWoong <ansghddnd12@naver.com>
2023-10-23 13:45:06 +0900

    Feat : 회원가입 페이지 UI 완성

--- front/knocknock/lib/screens/signup.dart
+++ front/knocknock/lib/screens/signup.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:knocknock/screens/login.dart';
+import 'package:remedi_kopo/remedi_kopo.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -10,12 +13,30 @@ class Signup extends StatefulWidget {
 TextEditingController emailController = TextEditingController();
 TextEditingController verificationController = TextEditingController();
 TextEditingController passwordController = TextEditingController();
-TextEditingController cardPasswordConfirmController = TextEditingController();
-TextEditingController cardNickNameController = TextEditingController();
+TextEditingController passwordConfirmController = TextEditingController();
+TextEditingController nicknameController = TextEditingController();
+TextEditingController addressController = TextEditingController();
 final _formKey = GlobalKey<FormState>();
-
+Map<String, String> formData = {};
 final emailRegex = RegExp(
     r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+void searchAddress(BuildContext context) async {
+  KopoModel? model = await Navigator.push(
+    context,
+    CupertinoPageRoute(
+      builder: (context) => RemediKopo(),
+    ),
+  );
+
+  if (model != null) {
+    final address = model.address ?? '';
+    addressController.value = TextEditingValue(
+      text: address,
+    );
+    formData['address'] = address;
+  }
+}
 
 class _SignupState extends State<Signup> {
   @override
@@ -27,7 +48,7 @@ class _SignupState extends State<Signup> {
         child: Column(
           children: [
             Container(
-              height: MediaQuery.of(context).size.height * 0.1,
+              height: MediaQuery.of(context).size.height * 0.05,
             ),
             Container(
               height: MediaQuery.of(context).size.height * 0.2,
@@ -41,79 +62,293 @@ class _SignupState extends State<Signup> {
               ),
             ),
             Container(
-              height: MediaQuery.of(context).size.height * 0.7,
+              height: MediaQuery.of(context).size.height * 0.75,
               width: 300,
               alignment: Alignment.topCenter,
               child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      Stack(
-                        alignment: Alignment.centerRight,
-                        children: [
-                          TextFormField(
-                            controller: emailController,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return '이메일을 입력하세요.';
-                              } else if (!emailRegex.hasMatch(value)) {
-                                return '유효한 이메일을 입력하세요.';
-                              }
-                              return null;
-                            },
-                            decoration: InputDecoration(
-                              focusedBorder: OutlineInputBorder(
-                                borderSide:
-                                    const BorderSide(color: Colors.green),
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide.none,
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              contentPadding: const EdgeInsets.symmetric(
-                                  vertical: 20, horizontal: 15),
-                              labelText: '이메일',
-                              fillColor: Colors.grey[200],
-                              filled: true,
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Stack(
+                      alignment: Alignment.centerRight,
+                      children: [
+                        TextFormField(
+                          controller: emailController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return '이메일을 입력하세요.';
+                            } else if (!emailRegex.hasMatch(value)) {
+                              return '유효한 이메일을 입력하세요.';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "유효한 이메일을 입력해주세요",
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(color: Colors.green),
+                              borderRadius: BorderRadius.circular(15.0),
                             ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 15),
+                            labelText: '이메일',
+                            fillColor: Colors.grey[200],
+                            filled: true,
                           ),
-                          TextButton(
-                            onPressed: () {},
-                            child: const Text('인증'),
-                          ),
-                        ],
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text('인증'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true, // 비밀번호 숨기기 옵션
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return '비밀번호를 입력하세요.';
+                        } else if (value.length < 8 || value.length > 16) {
+                          return '비밀번호는 8~16자리 사이여야 합니다.';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: "8~16자리 사이로 입력해주세요",
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 15),
+                        labelText: '비밀번호',
+                        fillColor: Colors.grey[200],
+                        filled: true,
                       ),
-                      const SizedBox(height: 20),
-                      TextFormField(
-                        controller: passwordController,
-                        obscureText: true, // 비밀번호 숨기기 옵션
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return '비밀번호를 입력하세요.';
-                          } else if (value.length < 8 || value.length > 16) {
-                            return '비밀번호는 8~16자리 사이여야 합니다.';
-                          }
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.green),
-                            borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      controller: passwordConfirmController,
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return '비밀번호를 입력해주세요.';
+                        } else if (value != passwordController.text) {
+                          return '비밀번호가 일치하지 않습니다.';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: "비밀번호를 확인해주세요",
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 15),
+                        labelText: '비밀번호 확인',
+                        fillColor: Colors.grey[200],
+                        filled: true,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      controller: nicknameController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return '닉네임을 입력해주세요.';
+                        } else if (value.length < 2 || value.length > 8) {
+                          return '닉네임은 2~5자리 사이여야 합니다.';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: "2~5자리 사이로 입력해주세요",
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 15),
+                        labelText: '닉네임',
+                        fillColor: Colors.grey[200],
+                        filled: true,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Stack(
+                      alignment: Alignment.centerRight,
+                      children: [
+                        TextFormField(
+                          controller: addressController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return '주소를 입력하세요';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "도로명 주소를 입력해주세요",
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(color: Colors.green),
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 15),
+                            labelText: '주소',
+                            fillColor: Colors.grey[200],
+                            filled: true,
                           ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            searchAddress(context);
+                          },
+                          child: const Text('검색'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 300,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.green[900],
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.6),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: const Offset(0, 5),
+                            ),
+                          ],
+                        ),
+                        child: const Text(
+                          '회원가입',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
                           ),
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 15),
-                          labelText: '비밀번호',
-                          fillColor: Colors.grey[200],
-                          filled: true,
                         ),
                       ),
-                    ],
-                  )),
+                    ),
+                    const SizedBox(height: 20),
+                    const Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 1,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Text('간편 회원가입'),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 7, vertical: 12),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.6),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: const Offset(0, 5),
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              width: 30,
+                              height: 30,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/google.png'),
+                                ),
+                              ),
+                            ),
+                            const Text(
+                              'Google 이메일로 회원가입',
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w400),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    // const SizedBox(
+                    //   height: 10,
+                    // ),
+                    // Row(
+                    //   mainAxisAlignment: MainAxisAlignment.center,
+                    //   children: [
+                    //     const Text(
+                    //       '이미 계정이 있으신가요?',
+                    //       style: TextStyle(
+                    //           color: Colors.grey, fontWeight: FontWeight.w600),
+                    //     ),
+                    //     TextButton(
+                    //       onPressed: () {
+                    //         Navigator.push(
+                    //           context,
+                    //           MaterialPageRoute(
+                    //               builder: (context) => const Login()),
+                    //         );
+                    //       },
+                    //       child: Text(
+                    //         '로그인',
+                    //         style: TextStyle(
+                    //           color: Colors.green[900],
+                    //           fontWeight: FontWeight.w600,
+                    //         ),
+                    //       ),
+                    //     ),
+                    //   ],
+                    // ),
+                  ],
+                ),
+              ),
             )
           ],
         ),
---
+++ front/knocknock/lib/services/user_service.dart
@@ -0,0 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:async';
+import 'package:http_interceptor/http_interceptor.dart';
