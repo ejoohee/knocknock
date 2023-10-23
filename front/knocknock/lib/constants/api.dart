0303fbca170648a880ce4fedcfedbe59d56913c1
MoonHongWoong <ansghddnd12@naver.com>
2023-10-23 14:08:47 +0900

    Feat : HttpInterceptor 구현

---
+++ front/knocknock/lib/constants/api.dart
@@ -0,0 +1,4 @@
+class Api {
+  // static const BASE_URL = "https://richminime.site/api";
+  static const BASE_URL = "http://10.0.2.2:8080/api";
+}
--- front/knocknock/lib/services/user_service.dart
+++ front/knocknock/lib/services/user_service.dart
@@ -3,3 +3,9 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:async';
 import 'package:http_interceptor/http_interceptor.dart';
+import 'package:knocknock/constants/api.dart';
+
+const String baseUrl = Api.BASE_URL;
+const storage = FlutterSecureStorage();
+
+class UserService {}
