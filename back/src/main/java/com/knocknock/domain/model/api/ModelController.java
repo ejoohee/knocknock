e743e7dc85568e2b886b5c2e775771923a399d24
조은비 <harunz@naver.com>
2023-10-24 10:36:09 +0900

    Feat(MyModel) : 내 가전제품 등록 api 추가

--- back/src/main/java/com/knocknock/domain/model/api/ModelController.java
+++ back/src/main/java/com/knocknock/domain/model/api/ModelController.java
@@ -1,10 +1,14 @@
 package com.knocknock.domain.model.api;
 
+import com.knocknock.domain.model.dto.request.AddMyModelReqDto;
 import com.knocknock.domain.model.dto.response.FindModelListResDto;
 import com.knocknock.domain.model.dto.response.FindModelResDto;
 import com.knocknock.domain.model.service.ModelService;
+import com.knocknock.domain.model.service.MyModelService;
+import com.knocknock.global.dto.MessageDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +20,7 @@ import java.util.List;
 public class ModelController {
 
     private final ModelService modelService;
+    private final MyModelService myModelService;
 
     @Operation(
             summary = "가전제품 목록 조회하기",
@@ -34,5 +39,18 @@ public class ModelController {
     public ResponseEntity<FindModelResDto> findMode(@PathVariable long modelId) {
         return ResponseEntity.ok(modelService.findModel(modelId));
     }
+
+
+    @Operation(
+            summary = "내 가전제품 등록하기",
+            description = "내 가전제품을 등록합니다."
+    )
+    @PostMapping("/my")
+    public ResponseEntity<MessageDto> addMyModel(@RequestBody AddMyModelReqDto addMyModelReqDto) {
+        myModelService.addMyModel(addMyModelReqDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(MessageDto.message("내 가전제품 등록 완료"));
+    }
+
 
 }
