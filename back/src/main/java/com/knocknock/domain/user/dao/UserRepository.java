e23b4915c7a3838878c659513f97ad1fd8ac65d0
이주희 <joo9837@naver.com>
2023-10-24 17:59:35 +0900

    [164]Feat(UserSocial): repo 구현
    - findByEmailAndIsSocial 구현

--- back/src/main/java/com/knocknock/domain/user/dao/UserRepository.java
+++ back/src/main/java/com/knocknock/domain/user/dao/UserRepository.java
@@ -9,5 +9,6 @@ public interface UserRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByEmail(String email);
 
+    Optional<Users> findByEmailAndIsSocial(String email, boolean isSocial);
 
 }
