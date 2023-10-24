6c680f5ce62016d91857ca3f4612e474f90c4859
이주희 <joo9837@naver.com>
2023-10-24 18:01:44 +0900

    [164]Feat(UserSocial): resDto 구현

---
+++ back/src/main/java/com/knocknock/domain/user/dto/response/SocialLoginResDto.java
@@ -0,0 +1,15 @@
+package com.knocknock.domain.user.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SocialLoginResDto {
+
+    private String accessToken;
+    private String refreshToken;
+    private String email;
+    private String nickname;
+    private String address;
+}
