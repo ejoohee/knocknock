f5b063d97e9b6bada656fc85168929d3eec4018e
조은비 <harunz@naver.com>
2023-10-24 11:01:58 +0900

    Feat(MyModel) : 내 가전제품 삭제 쿼리 추가

--- back/src/main/java/com/knocknock/domain/model/dao/MyModelRepository.java
+++ back/src/main/java/com/knocknock/domain/model/dao/MyModelRepository.java
@@ -2,8 +2,20 @@ package com.knocknock.domain.model.dao;
 
 import com.knocknock.domain.model.domain.MyModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MyModelRepository extends JpaRepository<MyModel, Long> {
 
+    // userId와 modelId로 회원이 삭제하려는 가전제품을 찾아 삭제
+    @Modifying
+    @Query(value = "DELETE FROM MyModel mm WHERE mm.user.userId = :userId AND mm.model.id = :modelId")
+    void deleteByUserAndModel(long userId, long modelId);
+
+    // 내가 등록한 가전제품 목록 조회
+    @Query(value = "SELECT mm FROM MyModel mm WHERE mm.user.userId = :userId")
+    List<MyModel> findAllByUser(long userId);
 
 }
