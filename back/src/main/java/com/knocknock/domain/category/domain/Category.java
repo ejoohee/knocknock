f10802a74537896f3f9a2306e8971cbca2f9508a
조은비 <harunz@naver.com>
2023-10-25 14:21:53 +0900

    [226] Fix(Category) : costUnit 컬럼 추가

--- back/src/main/java/com/knocknock/domain/category/domain/Category.java
+++ back/src/main/java/com/knocknock/domain/category/domain/Category.java
@@ -1,6 +1,7 @@
 package com.knocknock.domain.category.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,11 +19,11 @@ public class Category {
     @Column(length = 40, name = "category_name", nullable = false)
     String name;
 
-    @Column(length = 40, name = "usage")
-    String usage;
+    @Column(length = 40, name = "usage1")
+    String usage1;
 
-    @Column(length = 40, name = "usage_unit")
-    String usageUnit;
+    @Column(length = 40, name = "usage_unit1")
+    String usageUnit1;
 
     @Column(length = 40, name = "usage2")
     String usage2;
@@ -39,5 +40,23 @@ public class Category {
     // 에너지 비용 단위
     @Column(length = 40, name = "cost_unit", nullable = false)
     String costUnit;
+
+    // 에너지 비용 단위
+    @Column(length = 40, name = "co2_unit", nullable = false)
+    String co2Unit;
+
+
+    @Builder
+    public Category(String name, String usage1, String usageUnit1, String usage2, String usageUnit2, String usage3, String usageUnit3, String costUnit, String co2Unit) {
+        this.name = name;
+        this.usage1 = usage1;
+        this.usageUnit1 = usageUnit1;
+        this.usage2 = usage2;
+        this.usageUnit2 = usageUnit2;
+        this.usage3 = usage3;
+        this.usageUnit3 = usageUnit3;
+        this.costUnit = costUnit;
+        this.co2Unit = co2Unit;
+    }
 
 }
