a3769fcd994354ab78fd7c061d65482197c66308
이주희 <joo9837@naver.com>
2023-10-26 15:41:39 +0900

    [181]Feat(UserSocial): SocialLoginResDto userType 추가

--- back/src/main/java/com/knocknock/domain/user/dto/response/SocialLoginResDto.java
+++ back/src/main/java/com/knocknock/domain/user/dto/response/SocialLoginResDto.java
@@ -6,10 +6,10 @@ import lombok.Data;
 @Data
 @Builder
 public class SocialLoginResDto {
-
     private String accessToken;
     private String refreshToken;
     private String email;
+    private String userType;
     private String nickname;
     private String address;
 }
