897469855dee1819414fb256bfc465b1c0ca1f6f
dbflskfl1004 <dbflskfl1004@naver.com>
2023-11-01 16:55:37 +0900

    Fix swaggerConfig : resolve conflict

--- back/src/main/java/com/knocknock/global/config/SwaggerConfig.java
+++ back/src/main/java/com/knocknock/global/config/SwaggerConfig.java
@@ -1,52 +1,42 @@
 package com.knocknock.global.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spi.service.contexts.SecurityContextBuilder;
-import springfox.documentation.spring.web.plugins.Docket;
+
 
 @Configuration
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
-//    @Bean
-//    public GroupedOpenApi chatOpenApi() {
-//        String[] paths = {"/**"};
-//
-//        return GroupedOpenApi.builder()
-//                .group("Knocknock Mobile Service API v1")
-//                .pathsToMatch(paths)
-//                .build();
-//    }
-
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.knocknock"))
-                .paths(PathSelectors.ant("/api/**")).build();
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/**"};
 
-    }
-
-    // JWT SecurityContext 구성
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
+        return GroupedOpenApi.builder()
+                .group("Knocknock Mobile Service API v1")
+                .pathsToMatch(paths)
                 .build();
     }
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .securityContexts(Arrays.asList(securityContext()))
+//                .securitySchemes(Arrays.asList(apiKey()))
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.knocknock"))
+//                .paths(PathSelectors.ant("/api/**")).build();
+//
+//    }
+//
+//    // JWT SecurityContext 구성
+//    private SecurityContext securityContext() {
+//        return SecurityContext.builder()
+//                .securityReferences(defaultAuth())
+//                .build();
+//    }
 
 
 
