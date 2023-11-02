5b39b4217b729ecf01f2b907e30924cc0a37d55e
dbflskfl1004 <dbflskfl1004@naver.com>
2023-11-02 17:50:52 +0900

    Fix user : checkPassword / dto에 변수가 1개면 반드시 기본 생성자 필요(없으면 JsonParsingError 발생)

--- back/src/main/java/com/knocknock/domain/user/dto/password/PasswordReqDto.java
+++ back/src/main/java/com/knocknock/domain/user/dto/password/PasswordReqDto.java
@@ -1,7 +1,5 @@
 package com.knocknock.domain.user.dto.password;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,5 +7,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PasswordReqDto {
 
-    private String password;
+    private String password; // 변수가 하나인 dto는 requestBody로 사용할떄 기본생성자가 반드시 필요함
+    // 안그러면 JsonParsingError가 납니다.
 }
