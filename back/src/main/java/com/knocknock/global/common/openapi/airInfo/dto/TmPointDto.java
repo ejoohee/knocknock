34e3dc0169dda6fd45d23d3b994a823689ee2af6
yoonoi <dbddbd39@naver.com>
2023-11-11 12:15:08 +0900

    Feat airInfo : 에러나서 재생성중

--- back/src/main/java/com/knocknock/global/common/openapi/airInfo/dto/TmPointDto.java
+++ back/src/main/java/com/knocknock/global/common/openapi/airInfo/dto/TmPointDto.java
@@ -1,4 +1,21 @@
 package com.knocknock.global.common.openapi.airInfo.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class TmPointDto {
+
+    private String tmX;
+    private String tmY;
+
+//    public static TmPointDto jsonToDto(JSONObject object) {
+//        return TmPointDto.builder()
+//                .tmX(object.get("tmX").toString())
+//                .tmY(object.get("tmY").toString())
+//                .build();
+//    }
+
+
 }
