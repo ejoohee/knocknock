cacf33757ece4cfbfb387641733d5ce4098e6d53
yoonoi <dbddbd39@naver.com>
2023-11-11 14:02:24 +0900

    Fix jsonObject : json-simple -> javax

--- back/src/main/java/com/knocknock/global/common/openapi/airInfo/AirInfoController.java
+++ back/src/main/java/com/knocknock/global/common/openapi/airInfo/AirInfoController.java
@@ -1,4 +1,28 @@
 package com.knocknock.global.common.openapi.airInfo;
 
+import com.knocknock.global.common.openapi.airInfo.dto.AirInfoReqDto;
+import com.knocknock.global.common.openapi.airInfo.dto.AirInfoResDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/air-info")
 public class AirInfoController {
+
+    private final String ACCESS_TOKEN = "accessToken";
+    //    private final String ACCESS_TOKEN = "Authorization";
+    private final AirInfoService airInfoService;
+
+    @GetMapping
+    public ResponseEntity<AirInfoResDto> getAirInfoByRegion(AirInfoReqDto reqDto) throws IOException {
+        return ResponseEntity.ok(airInfoService.getAirInfoByRegion(reqDto));
+    }
+
+
 }
--- back/src/main/java/com/knocknock/global/common/openapi/airInfo/dto/AirInfoResDto.java
+++ back/src/main/java/com/knocknock/global/common/openapi/airInfo/dto/AirInfoResDto.java
@@ -3,6 +3,9 @@ package com.knocknock.global.common.openapi.airInfo.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.json.JsonObject;
+import java.util.Optional;
+
 @Data
 @Builder
 public class AirInfoResDto {
@@ -28,27 +31,33 @@ public class AirInfoResDto {
     private String 통합대기환경수치;
     private String 통합대기환경지수;
 
-//    public static AirInfoResDto jsonToDto(JSONObject object) {
+    public static AirInfoResDto jsonToDto(JsonObject object) {
 //        // 측정 오류가 있을 시
-//        if(object.get("coFlag") != null)
+//        if(object.get("coFlag") != null || !object.get("coFlag").equals("null")){
+//            System.out.println("비정상작동");
+//
 //            return AirInfoResDto.builder()
 //                    .측정상태(object.get("coFlag").toString())
 //                    .build();
+//        }
 //
-//        // 정상작동을 하더라도 null이 나오는 경우가 있어서 Optional로 대체
-//        return AirInfoResDto.builder()
-//                .측정상태("정상측정")
-//                .시간(Optional.ofNullable(object.get("dataTime")).map(Object::toString).orElse(null))
-//                .미세먼지농도(Optional.ofNullable(object.get("pm10Value")).map(Object::toString).orElse(null))
-//                .초미세먼지농도(Optional.ofNullable(object.get("pm25Value")).map(Object::toString).orElse(null))
-//                .오존농도(Optional.ofNullable(object.get("o3Value")).map(Object::toString).orElse(null))
-//                .일산화탄소농도(Optional.ofNullable(object.get("coValue")).map(Object::toString).orElse(null))
-//                .아황산가스농도(Optional.ofNullable(object.get("so2Value")).map(Object::toString).orElse(null))
-//                .이산화질소농도(Optional.ofNullable(object.get("no2Value")).map(Object::toString).orElse(null))
-//                .통합대기환경수치(Optional.ofNullable(object.get("khaiValue")).map(Object::toString).orElse(null))
-//                .통합대기환경지수(Optional.ofNullable(object.get("khaiGrade")).map(Object::toString).orElse(null))
-//                .build();
-//    }
+//
+//        System.out.println("정상작동");
+
+        // 정상작동을 하더라도 null이 나오는 경우가 있어서 Optional로 대체
+        return AirInfoResDto.builder()
+                .측정상태("정상측정")
+                .시간(Optional.ofNullable(object.getString("dataTime")).orElse(null))
+                .미세먼지농도(Optional.ofNullable(object.getString("pm10Value")).orElse(null))
+                .초미세먼지농도(Optional.ofNullable(object.getString("pm25Value")).orElse(null))
+                .오존농도(Optional.ofNullable(object.getString("o3Value")).orElse(null))
+                .일산화탄소농도(Optional.ofNullable(object.getString("coValue")).orElse(null))
+                .아황산가스농도(Optional.ofNullable(object.getString("so2Value")).orElse(null))
+                .이산화질소농도(Optional.ofNullable(object.getString("no2Value")).orElse(null))
+                .통합대기환경수치(Optional.ofNullable(object.getString("khaiValue")).orElse(null))
+                .통합대기환경지수(Optional.ofNullable(object.getString("khaiGrade")).orElse(null))
+                .build();
+    }
 
 
 }
